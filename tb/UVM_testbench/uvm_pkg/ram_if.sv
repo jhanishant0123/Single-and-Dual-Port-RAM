f2647ffb78ee46d979b03efe2e797a42b11dfda3
NISHANT KUMAR JHA <140664055+jhanishant0123@users.noreply.github.com>
2025-07-11 09:35:27 +0530

    Update ram_if.sv

--- tb/UVM_testbench/uvm_pkg/ram_if.sv
+++ tb/UVM_testbench/uvm_pkg/ram_if.sv
@@ -1,1 +1,17 @@
+interface ram_if #(parameter DATA_WIDTH = 8, ADDR_WIDTH = 4)(input logic clk);
+
+  logic rst;
+  // Port A
+  logic [ADDR_WIDTH-1:0] addr_a;
+  logic [DATA_WIDTH-1:0]  din_a;
+  logic we_a;
+  logic [DATA_WIDTH-1:0] dout_a;
+
+  // Port B
+  logic [ADDR_WIDTH-1:0] addr_b;
+  logic [DATA_WIDTH-1:0]  din_b;
+  logic we_b;
+  logic [DATA_WIDTH-1:0] dout_b;
+
+endinterface
 
